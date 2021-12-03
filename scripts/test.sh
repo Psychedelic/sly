475ea5be9bb3f5e60e2e92668349c128bcd26022
Nima Rasooli <nimarasooli1@gmail.com>
2021-12-03 17:52:56 +0330

    update test for sly_rust

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@ cargo build
 cd target/debug/ || exit
 # ./sly new --name sly_ft --template fungible_token
 # ./sly new --name sly_nft --template non_fungible_token
-./sly new --template rust_backend sly_rust
+./sly new --template rust_backend ../../../sly_rust
 # ./sly new --name sly_default
 cd sly_rust || exit
 ../sly build --with-mode release --all
